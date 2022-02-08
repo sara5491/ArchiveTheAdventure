14fa0d7d09ccd9accbf33a4f9659c989ed20a800
sara5491 <saraelinevans@gmail.com>
2022-02-08 19:50:37 +0000

    create add_photo template

--- app.py
+++ app.py
@@ -99,6 +99,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_photo")
+def add_photo():
+    return render_template("add_photo.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
