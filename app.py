2923f027d6332ae6bc0fe758b666c8cbeaf95efb
sara5491 <saraelinevans@gmail.com>
2022-02-10 21:57:06 +0000

    add search function

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_photos():
     return render_template("photos.html", photos=photos)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    photos = list(mongo.db.photos.find({"$text": {"$search": query}}))
+    return render_template("photos.html", photos=photos)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
